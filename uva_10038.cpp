0f2782583dcdbfc75bd87bafde383ac6e19cd166
Amin Samadi <aminsamadi21@gmail.com>
2017-01-17 22:41:15 +0330

    Update uva_10038.cpp

--- uva_10038.cpp
+++ uva_10038.cpp
@@ -6,30 +6,33 @@ int n,k;
 char c;
 bool flag=0;
 using namespace std;
-int main(){
-    vector<int>v;
-    vector<int>v1;
-    cin>>n;
-    for (int j = 0; j < n; ++j) {
-        cin>>k;
-        v.push_back(k);
-    }
-
-    for (int i = 0; i < n; ++i) {
-        v1.push_back(abs(v[i]-v[i+1]));
-    }
-    sort(v1.begin(),v1.end());
-    for (int i = 0; i < v1.size()-1; ++i) {
-        if(v1[i]==v1[i+1]){
-            flag=1;
-            break;
+int main() {
+    vector<int> v;
+    vector<int> v1;
+    while (cin >> n) {
+        for (int j = 0; j < n; ++j) {
+            cin >> k;
+            v.push_back(k);
         }
-        else
-            continue;
+
+        for (int i = 0; i < n-1; ++i) {
+            v1.push_back(abs(v[i] - v[i + 1]));
+        }
+        sort(v1.begin(), v1.end());
+        for (int i = 0; i < v1.size() - 1; ++i) {
+            if (v1[i] == v1[i + 1]) {
+                flag = 1;
+                break;
+            } else
+                continue;
+        }
+        if (flag == 1) {
+            cout << "Not jolly" << endl;
+        } else
+            cout << "Jolly" << endl;
+
+        flag=0;
+        v.clear();
+        v1.clear();
     }
-    if(flag==1){
-        cout<<"Not jolly"<<endl;
-    }
-    else
-        cout<<"Jolly"<<endl;
 }
