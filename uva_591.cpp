7bc01c433b40b88f0172d7370879201ce0248610
Amin Samadi <aminsamadi21@gmail.com>
2017-01-17 22:40:28 +0330

    Update uva_591.cpp

--- uva_591.cpp
+++ uva_591.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 int main() {
     int arr[50];
-    int n=0,sum=0;
-    cin>>n;
-    while(n){
+    int n=0,sum=0,k=0;
+    int a=1;
+    while(cin>>n && n){
+        sum = 0;
+        k=0;
         for (int i = 0; i < n; ++i) {
             cin>>arr[i];
             sum+=arr[i];
         }
         int average=sum/n;
-        int k=0;
         for (int j = 0; j < n ; ++j) {
             if(arr[j]>average) {
-                k += arr[j] - average;
+                k += (arr[j] - average);
             }
         }
-        cout<<"The minimum number of moves is "<<k<<endl;
-        k=0;
-        sum=0;
-        cin>>n;
+        cout<<"Set #"<<a<<endl;
+        cout<<"The minimum number of moves is "<<k<<"."<<endl<<endl;
+        a++;
 
     }
     return 0;
